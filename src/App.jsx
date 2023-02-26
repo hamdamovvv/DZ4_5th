a2cf8cf976961576afc5024fbd0b47a4dd67b6b5
hamdamovvv <akhmadkhamdamov000@gmail.com>
2023-02-26 21:32:09 +0600

    dz4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NumberList from "./components/NumberList/NumberList";
+
+function App() {
+
+  return (
+    <div className="App">
+      <NumberList/>
+    </div>
+  )
+}
+
+export default App
